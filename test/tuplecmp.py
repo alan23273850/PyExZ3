50fb109d418c4b96d0ab36090683e6a8f5d7b0d7
Wei-Lun Tsai <alan23273850@gmail.com>
2021-04-22 15:39:18 +0800

    make some auxiliary functions become inner for some testcases

--- test/tuplecmp.py
+++ test/tuplecmp.py
@@ -1,14 +1,13 @@
-def tuplelt(a, b):
-  a0, a1 = a
-  b0, b1 = b
-
-  if a0 < b0:
-    return True
-  elif a1 < b1:
-    return True
-  return False
-
 def tuplecmp(a0, a1, b0, b1):
+  def tuplelt(a, b):
+    a0, a1 = a
+    b0, b1 = b
+
+    if a0 < b0:
+        return True
+    elif a1 < b1:
+        return True
+    return False
   return tuplelt((a0, a1), (b0, b1))
 
 def expected_result():
--- test/unnecessary_condition4.py
+++ test/unnecessary_condition4.py
@@ -1,4 +1,15 @@
 def unnecessary_condition4(in1):
+    def op(in1):
+        if in1 < -10:
+            return 1
+        if in1 < -5:
+            return 2
+        if in1 < -3:
+            return 3
+        if in1 < 0:
+            return 4
+            
+        return 0
     v = op(in1)
 
     if in1 > 0:
@@ -6,18 +17,6 @@ def unnecessary_condition4(in1):
         return v + 10
     else:
         return v + 20
-        
-def op(in1):
-    if in1 < -10:
-        return 1
-    if in1 < -5:
-        return 2
-    if in1 < -3:
-        return 3
-    if in1 < 0:
-        return 4
-        
-    return 0
-    
+            
 def expected_result():
     return [10,20,21,22,23,24]
